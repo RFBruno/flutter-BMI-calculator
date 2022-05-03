e4cbd1f9fc2139338cd3b218845832cc7c9e34cf
RFBruno <bruno_rafael_08@hotmail.com>
2022-05-02 22:22:16 -0300

    imc logic

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,48 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  TextEditingController weightController = new TextEditingController();
+  TextEditingController heightController = new TextEditingController();
+  String _textInfo = 'Informe seus dados!';
+
+  void _resetFields(){
+    weightController.clear();
+    heightController.clear();
+    setState(() {
+      _textInfo = 'Informe seus dados!';
+      
+    });
+  }
+
+  void _calculator(){
+    setState(() {
+      double weight = double.parse(weightController.text);
+      double height = double.parse(heightController.text) / 100;
+      double imc = weight / (height * height);
+
+      
+      if(imc < 18.6){
+        _textInfo = "Abaixo do Peso (${imc.toStringAsPrecision(3)})";
+      }else if(imc >= 18.6 && imc < 24.9){
+        _textInfo = "Peso Ideal (${imc.toStringAsPrecision(3)})";
+      }else if(imc >= 24.9 && imc < 29.9){
+        _textInfo = "Levemente Acima do Peso (${imc.toStringAsPrecision(3)})";
+      }else if(imc >= 29.9 && imc < 34.9){
+        _textInfo = "Obesidade Grau I (${imc.toStringAsPrecision(3)})";
+      }else if(imc >= 34.9 && imc < 39.9){
+        _textInfo = "Obesidade Grau II (${imc.toStringAsPrecision(3)})";
+      }else if(imc >= 40){
+        _textInfo = "Obesidade Grau III (${imc.toStringAsPrecision(3)})";
+      }
+      
+    });
+
+
+  }
+
+  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,7 +68,7 @@ class _HomeState extends State<Home> {
         actions: [
           IconButton(
             onPressed:() {
-              
+              _resetFields();
             } ,
             icon: const Icon(Icons.refresh)
             )
@@ -34,7 +76,7 @@ class _HomeState extends State<Home> {
       ),
       backgroundColor: Colors.white,
       body: SingleChildScrollView(
-        padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+        padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
@@ -44,30 +86,32 @@ class _HomeState extends State<Home> {
               color: Colors.green,
               
             ),
-            const TextField(
+            TextField(
               keyboardType: TextInputType.number,
-              decoration: InputDecoration(
+              decoration: const InputDecoration(
                 labelText: 'Peso (kg)',
                 labelStyle: TextStyle(color: Colors.green)
               ),
               textAlign: TextAlign.center,
-              style: TextStyle(
+              style: const TextStyle(
                 color: Colors.green,
                 fontSize: 25
               ),
+              controller: weightController,
             ),
       
-            const TextField(
+            TextField(
               keyboardType: TextInputType.number,
-              decoration: InputDecoration(
+              decoration: const InputDecoration(
                 labelText: 'Altura (cm)',
                 labelStyle: TextStyle(color: Colors.green)
               ),
               textAlign: TextAlign.center,
-              style: TextStyle(
+              style: const TextStyle(
                 color: Colors.green,
                 fontSize: 25
               ),
+              controller: heightController,
             ),
             
             Padding(
@@ -75,9 +119,7 @@ class _HomeState extends State<Home> {
               child: Container(
                 height: 50,
                 child: ElevatedButton(
-                  onPressed: () {
-      
-                  },
+                  onPressed: _calculator,
                   child: const Text(
                     'Calcular',
                     style: TextStyle(
@@ -93,10 +135,10 @@ class _HomeState extends State<Home> {
               ),
             ),
       
-            const Text(
-              'Infor',
+            Text(
+              _textInfo,
               textAlign: TextAlign.center,
-              style: TextStyle(
+              style: const TextStyle(
                 color: Colors.green,
                 fontSize: 25
               ),
